4dd8ff4a013820a5ee310f36a030d31ea86155b1
Navaid Ali <68498876+PyNavaid@users.noreply.github.com>
2022-03-13 12:46:28 +0500

    Add files via upload

---
+++ Quiz-1/Q2.js
@@ -0,0 +1,16 @@
+function Palindrome(string) {
+
+    const len = string.length;
+    for (let i = 0; i < len / 2; i++) {
+
+        if (string[i] !== string[len - 1 - i]) {
+            return 'It is not a palindrome';
+        }
+    }
+    return 'It is a palindrome';
+}
+
+const string = prompt('Enter a string: ');
+const value = Palindrome(string);
+
+console.log(value);
